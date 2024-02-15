dce2a1efd5d5fac4ef3b9142e5ac9305718919f6
f00834653 <feyza.urkut1@huawei.com>
2024-02-15 11:18:02 +0300

    CMS-DEMO-3 # Add view binding to project

--- app/src/main/java/huawei/cmsdemo/main/ui/home/HomeScreen.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/home/HomeScreen.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import huawei.cmsdemo.main.R
+import huawei.cmsdemo.main.databinding.FragmentHomeScreenBinding
 
 class HomeScreen : Fragment() {
 
+    private lateinit var binding: FragmentHomeScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    ): View {
+        binding = FragmentHomeScreenBinding.inflate(inflater)
+        return binding.root
     }
 
 }
