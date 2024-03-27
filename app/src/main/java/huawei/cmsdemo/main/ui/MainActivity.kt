da2f2a52aeb62eb8045f11ea33e2b9625246d638
f00834653 <feyza.urkut1@huawei.com>
2024-03-27 08:43:17 +0300

    CMS-DEMO-7 # Move to ui package

--- app/src/main/java/huawei/cmsdemo/main/ui/MainActivity.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package huawei.cmsdemo.main
+package huawei.cmsdemo.main.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import huawei.cmsdemo.main.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
