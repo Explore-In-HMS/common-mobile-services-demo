aa5e847d3ee4149aa288bea093419e2cab1547bd
Emine INAN <emineinanx@gmail.com>
2024-06-12 11:51:26 +0300

    CMS-DEMO-12 # Update AdsScreen.kt

--- app/src/main/java/huawei/cmsdemo/main/ui/ads/AdsScreen.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/ads/AdsScreen.kt
@@ -16,6 +16,7 @@ import com.hms.lib.commonmobileservices.ads.rewarded.common.IRewardItem
 import com.hms.lib.commonmobileservices.ads.rewarded.common.RewardedAdLoadCallback
 import com.hms.lib.commonmobileservices.ads.rewarded.common.UserRewardEarnedListener
 import com.hms.lib.commonmobileservices.ads.rewarded.implementation.IRewardedAd
+import com.huawei.hms.ads.AdListener
 import com.huawei.hms.ads.AdParam
 import huawei.cmsdemo.main.R
 import huawei.cmsdemo.main.databinding.FragmentAdsScreenBinding
@@ -25,7 +26,6 @@ import huawei.cmsdemo.main.util.Constants.HMS_AD_ID_INTERSTITIAL
 import huawei.cmsdemo.main.util.Constants.HMS_AD_ID_REWARDED
 import huawei.cmsdemo.main.util.toastLong
 import huawei.cmsdemo.main.util.toastShort
-
 
 class AdsScreen : Fragment() {
     private lateinit var binding: FragmentAdsScreenBinding
@@ -42,6 +42,10 @@ class AdsScreen : Fragment() {
         with(binding) {
             btnBannerAds.setOnClickListener {
                 showBannerAd()
+            }
+
+            btnRewardedAds.setOnClickListener {
+                showRewardedAd()
             }
 
             btnInterstitialAds.setOnClickListener {
@@ -73,65 +77,65 @@ class AdsScreen : Fragment() {
         bannerView.adListener = adListener
     }
 
+    private fun showRewardedAd() {
+        showProgress()
+        requireContext().toastLong(getString(R.string.rewarded_ad_loading))
+        val adRequest = AdManagerAdRequest.Builder().build()
+        val adParam = AdParam.Builder().build()
+
+        RewardedAd.load(
+            requireContext(),
+            HMS_AD_ID_REWARDED,
+            GMS_AD_ID_REWARDED,
+            object : RewardedAdLoadCallback {
+                override fun onAdLoadFailed(adError: String) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.rewarded_ad_failed) + adError)
+                }
+
+                override fun onRewardedAdLoaded(rewardedAd: IRewardedAd) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.rewarded_ad_loaded))
+                    rewardedAd.show(requireActivity(), object : UserRewardEarnedListener {
+                        override fun onUserEarnedReward(item: IRewardItem) {
+                            requireContext().toastShort(
+                                getString(R.string.earned_reward_score) + item.getAmount()
+                                    .toString()
+                            )
+                        }
+                    })
+                }
+            },
+            adRequest,
+            adParam
+        )
+    }
+
     private fun showInterstitialAd() {
         showProgress()
         requireContext().toastLong(getString(R.string.interstitial_ad_loading))
         val adRequest = AdRequest.Builder().build()
         val adParam = AdParam.Builder().build()
 
-                InterstitialAd.load(
-                    requireContext(),
-                    HMS_AD_ID_INTERSTITIAL,
-                    GMS_AD_ID_INTERSTITIAL,
-                    adRequest,
-                    adParam,
-                    object :
-                        InterstitialAdLoadCallback {
-                        override fun onAdLoadFailed(adError: String) {
-                            hideProgress()
-                            requireContext().toastShort(getString(R.string.interstitial_ad_failed) + adError)
-                        }
+        InterstitialAd.load(
+            requireContext(),
+            HMS_AD_ID_INTERSTITIAL,
+            GMS_AD_ID_INTERSTITIAL,
+            adRequest,
+            adParam,
+            object :
+                InterstitialAdLoadCallback {
+                override fun onAdLoadFailed(adError: String) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.interstitial_ad_failed) + adError)
+                }
 
-                        override fun onInterstitialAdLoaded(interstitialAd: IInterstitialAd) {
-                            hideProgress()
-                            requireContext().toastShort(getString(R.string.interstitial_ad_loaded))
-                            interstitialAd.show(requireActivity())
-                        }
-                    })
-            }
-
-            btnRewardedAds.setOnClickListener {
-                showProgress()
-                requireContext().toastLong(getString(R.string.rewarded_ad_loading))
-                val adRequest = AdManagerAdRequest.Builder().build()
-                val adParam = AdParam.Builder().build()
-
-                RewardedAd.load(
-                    requireContext(),
-                    HMS_AD_ID_REWARDED,
-                    GMS_AD_ID_REWARDED,
-                    object : RewardedAdLoadCallback {
-                        override fun onAdLoadFailed(adError: String) {
-                            hideProgress()
-                            requireContext().toastShort(getString(R.string.rewarded_ad_failed) + adError)
-                        }
-
-                        override fun onRewardedAdLoaded(rewardedAd: IRewardedAd) {
-                            hideProgress()
-                            requireContext().toastShort(getString(R.string.rewarded_ad_loaded))
-                            rewardedAd.show(requireActivity(), object: UserRewardEarnedListener{
-                                override fun onUserEarnedReward(item: IRewardItem) {
-                                    requireContext().toastShort(getString(R.string.earned_reward_score) + item.getAmount().toString())
-                                }
-                            })
-                        }
-
-                    },
-                    adRequest,
-                    adParam
-                )
-            }
-        }
+                override fun onInterstitialAdLoaded(interstitialAd: IInterstitialAd) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.interstitial_ad_loaded))
+                    interstitialAd.show(requireActivity())
+                }
+            })
     }
 
     private fun showProgress() {
