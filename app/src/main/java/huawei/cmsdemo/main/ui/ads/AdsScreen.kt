1bb979ff43e74728f69159d660714f0653e8772f
Feyza Urkut <feyzaurkut1@gmail.com>
2024-07-29 10:39:47 +0300

    CMS-DEMO-34 # Update show banner ad function

--- app/src/main/java/huawei/cmsdemo/main/ui/ads/AdsScreen.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/ads/AdsScreen.kt
@@ -6,7 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.hms.lib.commonmobileservices.ads.banner.common.BannerAdLoadCallback
+import com.hms.lib.commonmobileservices.ads.banner.implementation.IBannerAd
 import com.hms.lib.commonmobileservices.ads.interstitial.InterstitialAd
 import com.hms.lib.commonmobileservices.ads.interstitial.common.InterstitialAdLoadCallback
 import com.hms.lib.commonmobileservices.ads.interstitial.implementation.IInterstitialAd
@@ -15,11 +16,8 @@ import com.hms.lib.commonmobileservices.ads.rewarded.common.IRewardItem
 import com.hms.lib.commonmobileservices.ads.rewarded.common.RewardedAdLoadCallback
 import com.hms.lib.commonmobileservices.ads.rewarded.common.UserRewardEarnedListener
 import com.hms.lib.commonmobileservices.ads.rewarded.implementation.IRewardedAd
-import com.hms.lib.commonmobileservices.ads.splash.SplashAd
 import com.hms.lib.commonmobileservices.ads.splash.common.SplashAdLoadCallback
 import com.hms.lib.commonmobileservices.ads.splash.implementation.ISplashAd
-import com.huawei.hms.ads.AdListener
-import com.huawei.hms.ads.AdParam
 import huawei.cmsdemo.main.R
 import huawei.cmsdemo.main.databinding.FragmentAdsScreenBinding
 import huawei.cmsdemo.main.util.Constants.GMS_AD_ID_INTERSTITIAL
@@ -91,26 +89,21 @@ class AdsScreen : Fragment() {
     }
 
     private fun showBannerAd() {
-        val bannerView = binding.hwBannerView
-        val adParam = AdParam.Builder().build()
-        bannerView.loadAd(adParam)
+        showProgress()
+        val bannerView = binding.cvBannerAd
+        bannerView.initialize(
+            object: BannerAdLoadCallback{
+                override fun onAdLoadFailed(adError: String) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.banner_ad_failed))
+                }
 
-        val adListener: AdListener = object : AdListener() {
-            override fun onAdLoaded() {
-                requireContext().toastShort(getString(R.string.banner_ad_loaded))
+                override fun onBannerAdLoaded(bannerAd: IBannerAd) {
+                    hideProgress()
+                    requireContext().toastShort(getString(R.string.banner_ad_loaded))
+                }
             }
-
-            override fun onAdFailed(errorCode: Int) {
-                requireContext().toastShort(getString(R.string.banner_ad_failed) + errorCode)
-            }
-
-            override fun onAdClosed() {
-                bannerView.destroy()
-                requireContext().toastShort(getString(R.string.banner_ad_closed))
-            }
-        }
-
-        bannerView.adListener = adListener
+        )
     }
 
     private fun showRewardedAd() {
