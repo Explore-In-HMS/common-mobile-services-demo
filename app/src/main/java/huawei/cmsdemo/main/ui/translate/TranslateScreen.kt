d2d31b200ca3521133d432941fa98fdc79f9f55e
Emine INAN <emineinanx@gmail.com>
2024-08-21 14:30:44 +0300

    CMS-DEMO-54 # Add showAlertDialog function

--- app/src/main/java/huawei/cmsdemo/main/ui/translate/TranslateScreen.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/translate/TranslateScreen.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import huawei.cmsdemo.main.R
 import huawei.cmsdemo.main.databinding.FragmentTranslateScreenBinding
+import huawei.cmsdemo.main.util.Util
+import huawei.cmsdemo.main.util.showAlertDialog
 
 
 class TranslateScreen : Fragment() {
@@ -16,7 +19,20 @@ class TranslateScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentTranslateScreenBinding.inflate(inflater)
+        showAlertDialog()
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun showAlertDialog() {
+        val servicesInfo =
+            Util.servicesInfoList.first { it.servicesName == getString(R.string.translate_kit) }
+        with(servicesInfo) {
+            requireContext().showAlertDialog(
+                title = servicesName,
+                desc = desc,
+                versions = version
+            )
+        }
     }
 }
