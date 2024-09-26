abd943424b1e0b05a4077768a78daf59f96637ca
Feyza Urkut <feyzaurkut1@gmail.com>
2024-09-26 08:35:41 +0300

    CMS-DEMO-69 # Create credit card scanner screen with logic

--- app/src/main/java/huawei/cmsdemo/main/ui/creditcard/CreditCardScannerScreen.kt
+++ app/src/main/java/huawei/cmsdemo/main/ui/creditcard/CreditCardScannerScreen.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.hms.lib.commonmobileservices.core.ResultData
+import com.hms.lib.commonmobileservices.creditcardscanner.CreditCardScanner
 import huawei.cmsdemo.main.R
 import huawei.cmsdemo.main.databinding.FragmentCreditCardScannerScreenBinding
 import huawei.cmsdemo.main.util.Util
@@ -19,6 +22,7 @@ class CreditCardScannerScreen : Fragment() {
     ): View {
         binding = FragmentCreditCardScannerScreenBinding.inflate(inflater)
         showAlertDialog()
+        initUI()
         return binding.root
     }
 
@@ -31,6 +35,49 @@ class CreditCardScannerScreen : Fragment() {
                 desc = desc,
                 versions = version
             )
+        }
+    }
+
+    private fun initUI() {
+        binding.btnStartScan.setOnClickListener {
+            scan()
+        }
+    }
+
+    private fun scan() {
+        CreditCardScanner.instance(requireContext())?.scan {
+            when (it) {
+                is ResultData.Success -> {
+                    binding.tvScanResult.text = getString(
+                        R.string.scan_result,
+                        it.data?.cardNumber,
+                        it.data?.expireDate
+                    )
+                    hideProgress()
+                }
+
+                is ResultData.Failed -> {
+                    binding.tvScanResult.text = it.error
+                    hideProgress()
+                }
+
+                is ResultData.Loading -> {
+                    showProgress()
+                }
+            }
+
+        }
+    }
+
+    private fun showProgress() {
+        with(binding) {
+            progressBar.isVisible = true
+        }
+    }
+
+    private fun hideProgress() {
+        with(binding) {
+            progressBar.isVisible = false
         }
     }
 }
